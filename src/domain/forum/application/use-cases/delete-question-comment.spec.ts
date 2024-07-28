7b8f9e7322b1503110e85b649fce21d7da9f7810
Diego Silva <diegprocopio98@gmail.com>
2024-07-28 15:57:13 -0300

    feat: added use case delete question comment

---
+++ src/domain/forum/application/use-cases/delete-question-comment.spec.ts
@@ -0,0 +1,42 @@
+import { InMemoryQuestionCommentRepository } from 'test/repositories/in-memory-questions-comments-repository'
+import { DeleteQuestionCommentUseCase } from './delete-question-comment'
+import { makeQuestionComment } from 'test/factories/make-question-comment'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
+let inMemoryQuestionCommentRepository: InMemoryQuestionCommentRepository
+let sut: DeleteQuestionCommentUseCase
+
+describe('Delete On Question Comment', () => {
+  beforeEach(() => {
+    inMemoryQuestionCommentRepository = new InMemoryQuestionCommentRepository()
+    sut = new DeleteQuestionCommentUseCase(inMemoryQuestionCommentRepository)
+  })
+
+  it('should be able to delete a question comment', async () => {
+    const questionComment = makeQuestionComment()
+
+    await inMemoryQuestionCommentRepository.create(questionComment)
+
+    await sut.execute({
+      questionCommentId: questionComment.id.toString(),
+      authorId: questionComment.authorId.toString(),
+    })
+
+    expect(inMemoryQuestionCommentRepository.items).toHaveLength(0)
+  })
+
+  it('should not be able to delete user question comment', async () => {
+    const questionComment = makeQuestionComment({
+      authorId: new UniqueEntityId('author-1'),
+    })
+
+    await inMemoryQuestionCommentRepository.create(questionComment)
+
+    expect(() => {
+      return sut.execute({
+        questionCommentId: questionComment.id.toString(),
+        authorId: 'author-2',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
