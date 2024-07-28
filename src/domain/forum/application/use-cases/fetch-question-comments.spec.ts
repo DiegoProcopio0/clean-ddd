b78bb27a200286f68dfc4d008f4fa427250a1e86
Diego Silva <diegprocopio98@gmail.com>
2024-07-28 16:30:02 -0300

    feat: added fetch question comment

---
+++ src/domain/forum/application/use-cases/fetch-question-comments.spec.ts
@@ -0,0 +1,54 @@
+import { FetchQuestionCommentsUseCase } from './fetch-question-comments'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { makeQuestionComment } from 'test/factories/make-question-comment'
+import { InMemoryQuestionCommentRepository } from 'test/repositories/in-memory-questions-comments-repository'
+
+let inMemoryQuestionCommentRepository: InMemoryQuestionCommentRepository
+let sut: FetchQuestionCommentsUseCase
+
+describe('Fetch Questions Comments', () => {
+  beforeEach(() => {
+    inMemoryQuestionCommentRepository = new InMemoryQuestionCommentRepository()
+    sut = new FetchQuestionCommentsUseCase(inMemoryQuestionCommentRepository)
+  })
+
+  it('should be able to fetch questions comments', async () => {
+    await inMemoryQuestionCommentRepository.create(
+      makeQuestionComment({
+        questionId: new UniqueEntityId('question-1'),
+      }),
+    )
+    await inMemoryQuestionCommentRepository.create(
+      makeQuestionComment({
+        questionId: new UniqueEntityId('question-1'),
+      }),
+    )
+    await inMemoryQuestionCommentRepository.create(
+      makeQuestionComment({
+        questionId: new UniqueEntityId('question-1'),
+      }),
+    )
+
+    const { questionComments } = await sut.execute({
+      questionId: 'question-1',
+      page: 1,
+    })
+
+    expect(questionComments).toHaveLength(3)
+  })
+
+  it('should be able to fetch paginated questions comments', async () => {
+    for (let i = 0; i <= 22; i++) {
+      await inMemoryQuestionCommentRepository.create(
+        makeQuestionComment({ questionId: new UniqueEntityId('question-1') }),
+      )
+    }
+
+    const { questionComments } = await sut.execute({
+      questionId: 'question-1',
+      page: 2,
+    })
+
+    expect(questionComments).toHaveLength(3)
+  })
+})
